d09984ccb1ac5e4bacf10e2079897102657116be
PiwkoM <piwkom44@gmail.com>
2024-02-26 12:23:00 +0100

    sections added

--- index.php
+++ index.php
@@ -5,16 +5,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css"/>
-    
-    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="favicon" href="icon.ico">
+    <title>Giełda Książkowa</title>
+
 </head>
 <body>
     
+    <header></header>
+    <div id="main"></div>
+    <?php
+        //TODO @AdamrePL: implement :3
+
+    ?>
+    <nav id="menu">
+        <p>Main</p>
+        <p>offer_browse</p>
+        <p>offer_create</p>
+        <p>my_offer</p>
+    </nav>
+    <footer>blah blah blah blah lorem ipsum blah blah blah blah.</footer>
 
     <noscript>
         Your pc has virus!
-        <!-- nooooo :( -->
     </noscript>
 </body>
 </html>
