b45bab7ff8241e92008971cd15acf0ca733394d5
AdamrePL <adam1234federowicz@gmail.com>
2024-03-01 10:52:46 +0100

    etetetete

--- user_page.php
+++ user_page.php
@@ -1,9 +1,8 @@
 <?php require_once "config.php"; ?>
 <!--user page-->
-
-<header>
     <?php
-            
-    
+        $query = mysqli_query($conn, "SELECT * FROM `users` WHERE uuid = 'tester#aA1'");
+        while ($result = mysqli_fetch_assoc($query)) {
+            print_r( $result );
+        }
     ?>
-</header>
