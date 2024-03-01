7eb82bc1141b9eb4a193e852923c531ca2e60f6d
PiwkoM <piwkom44@gmail.com>
2024-03-01 13:32:23 +0100

    user_page UserInfo template semi-done, show UID script commented to not embarass myself

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 // window.onload = alert("meta mis juevos en tu boca");
+
+//i have no idea what im doing :D
+// let A = document.querySelector("#ibox");
+// A.addEventListener("click", () => {
+//     A.blah blah blah show id then wait -> hide blah blah blah
+// })
