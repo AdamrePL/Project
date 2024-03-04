e7322e987bd76a7757d4f29c8db4d03390e00ca7
AdamrePL <adam1234federowicz@gmail.com>
2024-03-04 11:59:21 +0100

    added user register/login form

---
+++ src/access.php
@@ -0,0 +1,8 @@
+<form action="" class="user-form">
+    <input type="text" />
+    <input type="password" />
+</form>
+<form action="" class="user-form">
+    <input type="text" />
+    <input type="password" />
+</form>
