521ea85b79cc357aa7c3f7a00618d2e4620febaa
AdamrePL <adam1234federowicz@gmail.com>
2024-03-04 12:44:56 +0100

    bla bla refactor files for clean code

--- src/access.php
+++ src/access.php
@@ -1,8 +1,21 @@
-<form action="" class="user-form">
-    <input type="text" />
-    <input type="password" />
-</form>
-<form action="" class="user-form">
-    <input type="text" />
-    <input type="password" />
-</form>
+<head>
+    <link rel="stylesheet" href="../assets/css/access.css">
+</head>
+
+<div class="access-wrapper">
+    <div class="login-wrapper">
+        <form action="" class="user-form">
+            <input type="text" />
+            <input type="password" />
+        </form>
+    </div>
+    <div class="line"></div>
+    <div class="register-wrapper">
+        <form action="" class="user-form">
+            <input type="text" />
+            <input type="password" />
+        </form>
+    </div>
+</div>
+
+
