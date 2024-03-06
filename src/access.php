27b9b64f385c10029c0b8b3a34fc6322cb981962
AdamrePL <adam1234federowicz@gmail.com>
2024-03-06 13:05:14 +0100

    idk what i've changed, sooo gl

--- src/access.php
+++ src/access.php
@@ -1,24 +1,28 @@
+<?php require_once "../conf/config.php"; ?>
+
 <head>
     <link rel="stylesheet" href="../assets/css/access.css">
-    <title>Giełda - Zaloguj / Zarejestruj</title>
+    <title><?php echo SITENAME; ?> - Zaloguj / Zarejestruj</title>
 </head>
 
 <body>
     <div class="access-wrapper">
         <div>
-            <h1>login</h1>
+            <h1>Logowanie</h1>
             <form method="post" action="" class="user-form">
-                <input type="text" placeholder="User ID"/>
-                <input type="password" placeholder="hasło"/>
+                <input type="text" placeholder="ID użytkownika"/>
+                <input type="password" placeholder="Hasło (Jeżeli jest)"/>
                 <input type="submit" value="Zaloguj">
             </form>
         </div>
         <div class="line"></div>
         <div>
-            <h1>register</h1>
+            <h1>Rejestracja</h1>
             <form method="post" class="user-form">
-                <input type="text" placeholder="User ID"/>
-                <input type="password" placeholder="hasło"/>
+                <input type="text" placeholder="Nazwa użytkownika"/>
+                <input type="email" placeholder="Adres e-mail"/>
+                <input type="password" placeholder="Hasło (Opcjonalne)"/>
+                <input type="password" placeholder="Potwierdzenie hasła"/>
                 <input type="submit" value="Zarejestruj">
             </form>
         </div>
---
+++ src/booklist.php
@@ -0,0 +1,36 @@
+<?php require_once "../conf/config.php"; ?>
+
+<h1>Spis Książek</h1>
+<table>
+    <tr>
+        <td>Klasa</td>
+        <td>Przedmiot</td>
+        <td>Nazwa</td>
+        <td>Wydawnictwo</td>
+        <td>Autorzy</td>
+    </tr>
+    <?php 
+    $sql = "SELECT * FROM `booklist` ORDER BY `class` ASC, `subject`";
+    $query = mysqli_query($conn, $sql);
+    while ($result = mysqli_fetch_assoc($query)) {
+        echo '<tr>';
+        echo '<td>' . $result["class"] . '</td>';
+        echo '<td>' . $result["subject"] . '</td>';
+        echo '<td>' . $result["name"] . '</td>';
+        echo '<td>' . $result["publisher"] . '</td>';
+        echo '<td>';
+        $authors = explode(",", $result["authors"]);
+        for ($index = 0; $index < count($authors); $index++) {
+            if ($index != count($authors) - 1) {
+                echo $authors[$index] . ", <br>";
+            } else {
+                echo $authors[$index];
+            }
+        }
+        echo '</td>';
+        echo '</tr>';
+    }
+    ?>
+</table>
+
+<a href="booklist.json" download>Plik przygotowawczy dla wstawienia książek na stronę</a>
