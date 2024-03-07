3b8eee2d21507f8a94b10b0f001c4fed2f852ae8
PiwkoM <piwkom44@gmail.com>
2024-03-07 11:14:14 +0100

    Navigation bar finished (i hope so) ((what is graphical consistency))

--- src/navigation.php
+++ src/navigation.php
@@ -1,8 +1,18 @@
-<menu>
-    <a>Main</a>
-    <form>
-        <input type="text"><input type="submit">
-    </form>
-    <a>Profile</a>
-    <a>Add Offer</a>
-</menu>
+<head>
+    <title>yipee</title>
+    <link rel="stylesheet" type="text/css" href="../assets/css/navigation.css">
+</head>
+
+<body>
+    <menu>
+        <a href="../#nawigacja">Navigate</a>
+        <a href="../">Main</a>
+        <a href="booklist.php">Book List </a>
+        <a href="../#przegladaj"> Browse </a>
+        <span>
+            <!--//*!very important code, me no understand, but it work-->
+            <a href="access.php" id="log"><?php  echo !isset($_SESSION["uid"]) ? "Log In" : "Log Out"; ?></a>
+            <a href="profile.php" id="prof">Profile</a>
+        </span>
+    </menu>
+</body>
