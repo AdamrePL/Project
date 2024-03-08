93338ff3881e354fc4134425bda5d32bea96d6ec
AdamrePL <adam1234federowicz@gmail.com>
2024-03-08 15:01:47 +0100

    idk

--- controllers/login-controller.php
+++ controllers/login-controller.php
@@ -1,8 +1,5 @@
 <?php
 
-
-
-
 function log_out(): bool {
     if (empty($_SESSION)) {
         return false;
@@ -14,78 +11,14 @@ function log_out(): bool {
 }
 
 function update_last_login(mysqli $conn, string $username) {
-    $sql = "UPDATE users SET `last-login` = NOW() WHERE username = ?";
-    $stmt = mysqli_prepare($conn, $sql);
+    $sql = "UPDATE `users` SET `last-login` = NOW() WHERE username = ?";
+    $stmt = mysqli_stmt_prepare($conn, $sql);
     mysqli_stmt_bind_param($stmt, "s", $username);
     mysqli_stmt_execute($stmt);
     mysqli_stmt_close($stmt);
+    // dodać aby to robilo liste z ostatnimi logowaniami i dodawalo do tej listy ostatnie logowanie
+    // maksymalna ilosc ostatnich logowan = 3
+    // ostatnie logowanie dodawalo by na poczatek listy
 }
 
-function getAllSiblings(element) {
-    let childrens = [];
-    getAllChild(childrens, element.parentNode);
-}
-
-function getAllChild(array, div) {
-    for (let index = 0; index < div.childElementCount; index++) {
-        const element = div.children[index];
-        if (element.childElementCount > 0) {
-            getAllChild(array, element)
-        }
-        array.push(element);
-    }
-}
-
-function createParagraphForm() {
-    let p = document.querySelector(".footer");
-
-    let form = document.createElement("form");
-    form.setAttribute("id", "add-paragraph");
-    form.setAttribute("method", "POST");
-    form.onsubmit = (event) => {
-        event.preventDefault();
-        addParagraph();
-    }
-    
-    let textarea = document.createElement("textarea");
-    textarea.setAttribute("class", "paragraphs");
-    textarea.addEventListener('input', AdjustParagraphHeight);
-
-
-    let submitbutton = document.createElement("input");
-    submitbutton.setAttribute("type", "submit");
-    submitbutton.setAttribute("value", "Dodaj")
-
-
-    let clearbutton = document.createElement("input");
-    clearbutton.setAttribute("type", "reset");
-    clearbutton.setAttribute("value", "Wyczyść")
-
-    let cancelbutton = document.createElement("input");
-    cancelbutton.setAttribute("type", "button");
-    cancelbutton.setAttribute("value", "Anuluj");
-    cancelbutton.onclick = () => {
-        form.remove();
-    }
-
-    p.parentNode.insertBefore(form, p);
-    form.appendChild(textarea);
-    form.appendChild(cancelbutton);
-    form.appendChild(clearbutton);
-    form.appendChild(submitbutton);
-
-
-    function addParagraph() {
-        var form = document.querySelector("#add-paragraph");
-        let content = form.firstChild.innerHTML;
-    
-        const xhr = new XMLHttpRequest();
-        xhr.open("POST", "addparagraph.php", true);
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.onload = () => {
-            form.remove();
-        }
-        xhr.send(content)
-    }
-
-    $conn -> close();
+$conn -> close();
