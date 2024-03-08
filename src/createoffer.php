8f3c5eba654f69d735433e9673d2e9b0ac991597
PiwkoM <piwkom44@gmail.com>
2024-03-08 10:58:01 +0100

    g

--- src/createoffer.php
+++ src/createoffer.php
@@ -1,7 +1,13 @@
+<?php require_once "../conf/config.php"; ?>
+
 <div class="offerCreate">
     <h1>Testing</h1>
     <form>
         <input type="text" placeholder="Book title"/>
+        <input type="checkbox">fone</input>
+        <input type="checkbox">mail</input>
+        <input type="checkbox">the cord</input>
+        <input type="date"/>
         <select name="przedmioty">
             <?php $sql = "SELECT DISTINCT `subject` FROM booklist;";
                 $query = mysqli_query($conn, $sql);
@@ -10,8 +16,9 @@
                 }
             ?>
         </select>
+
         <!-- <input type="text" placeholder="Stan Książki"/> -->
-        <input type="submit" value="Create Offer"/>
-        <input type="reset" value="Reset"/>
+        <input type="submit" value="Create Offer" />
+        <input type="reset" value="Reset" />
     </form>
 </div>
