a22546acdd987cf0ff474c43fc6670bb59145015
PiwkoM <piwkom44@gmail.com>
2024-03-11 10:56:22 +0100

    update: still losing my mind

--- controllers/offer-controller.php
+++ controllers/offer-controller.php
@@ -1,6 +1,6 @@
 <?php
 
-require "config.php";
+require "../conf/config.php";
 $stmt = mysqli_stmt_init($conn);
 
 $title = $_POST["book"];
@@ -13,27 +13,13 @@ $dc = $_POST["discord"];
 $subject = $_POST["subjects"];
 
 
-// get info about book from booklist.json -> SOMEHOW fetch book depending on SOMETHING from booklist
-// -> turn fetched data into $Book encoded json array with said values -> insert it into offers table 
-//**god knows how im going to do like half of those steps lol
-file_get_contents("../assets/downloads/booklist.json");
-json_decode(); //*!THIS IS A RABBIT HOLE I AM VERY SCARED TO DIVE INTO.
+//get author,publisher, and class info from booklist.json(match book by title probably) -> if appears in multiple classes, set class to "multiple" (temp value)
+//-> when fetched, assign 
 
-//*!encode to json for db insert
-//TODO: figure out how to add the img:["",""] encode
-$Book = json_encode(array(
-    "name"=>$title,
-    "author"=>$author,
-    "publisher"=>$publisher,
-    "subject"=>$subject,
-    "class"=>$class,
-    "price"=>$price,
-    "quality"=>$quality,
-    "note"=>$desc
-    ), JSON_PRETTY_PRINT);
-
-var_dump($Book);
+// $json_data = file_get_contents("../assets/downloads/booklist.json");
+// json_decode($json_data,true); //*!THIS IS A RABBIT HOLE I AM VERY SCARED TO DIVE INTO.
 $status = array("active","inactive","terminated");
+
 
 $file = $_FILES['image'];
 $fileName = $file['name'];
@@ -41,6 +27,12 @@ $fileTempName = $file['tmp_name'];
 $fileSize = $file['size'];
 $fileError = $file['error'];
 $fileType = $file['type'];
+
+//! --INSERT THE FOLLOWING SOMEWHERE HERE--
+//!
+//! $tempSolution = [];
+//! array_push($tempSolution,fileName);
+//!
 
 $fileExt = strtolower(end(explode('.', $fileName)));
 $allowed = array('png', 'jpg', 'jpeg');
@@ -56,16 +48,29 @@ if (in_array($fileExt, $allowed)) {
     }
 }
 
+
+
 $ext = "png";
-foreach (glob("../assets/img/product-images/*.$ext") as $file) {
+foreach (glob("../_user/images/*.$ext") as $file) {
+} // odczyt 
 
-} // odczyt plikow
-//*?this only handles one file, no?
-//*?id say sth like "oh you can just rename file to offer id and everythings gonna be fine" but i'm too inept at backend so what do i know right
+//*!encode to json for db insert
+$Book = json_encode(array(
+    "name"=>$title,
+    "author"=>$author,
+    "publisher"=>$publisher,
+    "subject"=>$subject,
+    "class"=>$class,
+    "price"=>$price,
+    "quality"=>$quality,
+    "note"=>$desc,
+    "img"=>[$tempSolution[0],$tempSolution[1]]
+    ), JSON_PRETTY_PRINT);
 
+var_dump($Book);
 
 
 //*!remember to add sesh uid, dont be a bozo
 //*leaving multiple blanks due to db uncertainty lole, also commented because i am SCARED to insert anything for now
-//*! $sql = "INSERT INTO `offers` VALUES('','$_SESSION,'','','NOW()','DATE_ADD(NOW(),INTERVAL 14 DAY)','$status[1]','$phone','$email','$dc')";
+//*! $sql = "INSERT INTO `offers` VALUES('','$_SESSION,'','','NOW()','DATE_ADD(NOW(),INTERVAL 14 DAY)','$status[0]','$phone','$email','$dc')";
 //*! mysqli_query($conn,"$sql");
