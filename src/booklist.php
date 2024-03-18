3261d874e3576451b2a637752ad13b5a50ce7f69
AdamrePL <adam1234federowicz@gmail.com>
2024-03-18 23:18:39 +0100

    better booklist, wider tos

--- src/booklist.php
+++ src/booklist.php
@@ -5,67 +5,48 @@
 </head>
 
 <body>
-<h1>Spis Książek</h1>
+<!-- //& <section> Tutaj dodać filtrowanie książek po klasie i przedmiocie </section> -->
+<section id="booklist">
+    <h1>Spis Książek</h1>
+
     <?php 
-    $sql = "SELECT * FROM `booklist` ORDER BY `class` ASC, `subject`";
-    $query = mysqli_query($conn, $sql);
-    while ($result = mysqli_fetch_assoc($query)) {
-        //nah you know what fuck this list :sob:
-        echo '<div>';
-            echo '<span>';
-                echo '<p>'.$result["name"].'</p>';
-                echo '<p>Klasa - '.$result["class"].'</p>';
-            echo '</span>';
+        $sql = "SELECT * FROM `booklist` ORDER BY `class` ASC, `subject`";
+        $query = mysqli_query($conn, $sql);
+        while ($result = mysqli_fetch_assoc($query)) {
+            echo '<div class="card">';
+                echo '<span>';
+                    echo '<p>'. $result["name"] .'</p>';
+                    echo '<p>Klasa - '. $result["class"] .'</p>';
+                echo '</span>';
 
 
-            echo '<ul>';
-                echo'<li> Przedmiot: '.$result["subject"].'</li>';
-                echo'<li> Wydawnictwo: '.$result["publisher"].'</li>';
-                echo '<li> Autorzy: ';
-                $authors = explode(",", $result["authors"]);
-                for ($index = 0; $index < count($authors); $index++) {
-                    if ($index != count($authors) - 1) {
-                        echo $authors[$index] . ", ";
-                    } else {
-                        echo $authors[$index];
-                    }
-                }
-            echo '</li></ul>';
-        
-        echo '</div>';
-            // echo '<ul>';
-            //     echo '<li> Przedmiot: '.$result["subject"].'</li>';
-            //     echo '<li> Wydawnictwo: '.$result["publisher"].'</li>';
-            //     echo '<li> Autorzy: ';
-            //    
-            //     for ($index = 0; $index < count($authors); $index++) {
-            //         if ($index != count($authors) - 1) {
-            //             echo $authors[$index] . ", ";
-            //         } else {
-            //             echo $authors[$index];
-            //         }
-            //     }
-            //     echo '</li>';
-            // echo '</ul>';
-        echo '</table>';
-        // echo '<tr>';
-        // echo '<td>' . $result["class"] . '</td>';
-        // echo '<td>' . $result["subject"] . '</td>';
-        // echo '<td>' . $result["name"] . '</td>';
-        // echo '<td>' . $result["publisher"] . '</td>';
-        // echo '<td>';
-        // $authors = explode(",", $result["authors"]);
-        // for ($index = 0; $index < count($authors); $index++) {
-        //     if ($index != count($authors) - 1) {
-        //         echo $authors[$index] . ", <br>";
-        //     } else {
-        //         echo $authors[$index];
-        //     }
-        // }
-        // echo '</td>';
-        // echo '</tr>';
-    }
+                echo '<ul>';
+                    echo '<li> Przedmiot: '. $result["subject"] .'</li>';
+                    echo '<li> Wydawnictwo: '. $result["publisher"] .'</li>';
+                    echo '<li> Autorzy: ';
+                        $authors = explode(",", $result["authors"]);
+                        for ($index = 0; $index < count($authors); $index++) {
+                            if ($index != count($authors) - 1) {
+                                echo $authors[$index] . ", ";
+                            } else {
+                                echo $authors[$index];
+                            }
+                        }
+                    echo '</li>';
+                echo '</ul>';
+            echo '</div>';
+        }
     ?>
-<br>
-<a href="../booklist.json" download>Plik przygotowawczy dla wstawienia książek na stronę</a>
+</section>
+
+<section id="booklist-files">
+    <h1>Zawartość do pobrania</h1>
+    <a href="../assets/downloads/booklist.json" download>Uzupełniony - przykładowy plik przygotowawczy</a>
+    <a href="../assets/downloads/booklist_template.json" download>Plik przygotowawczy dla wstawienia książek na stronę</a>
+</section>
+
+<?php
+    include_once "footer.php";
+?>
+
 </body>
