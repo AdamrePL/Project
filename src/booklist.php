21f629393ffe83a548e146053fd08a23b66581b6
PiwkoM <piwkom44@gmail.com>
2024-03-19 10:49:35 +0100

    minor code cleanup waaaaah

--- src/booklist.php
+++ src/booklist.php
@@ -7,64 +7,32 @@
 <body>
 <h1>Spis Książek</h1>
     <?php 
-    $sql = "SELECT * FROM `booklist` ORDER BY `class` ASC, `subject`";
-    $query = mysqli_query($conn, $sql);
-    while ($result = mysqli_fetch_assoc($query)) {
-        //nah you know what fuck this list :sob:
-        echo '<div>';
-            echo '<span>';
-                echo '<p>'.$result["name"].'</p>';
-                echo '<p>Klasa - '.$result["class"].'</p>';
-            echo '</span>';
+        $sql = "SELECT * FROM `booklist` ORDER BY `class` ASC, `subject`";
+        $query = mysqli_query($conn, $sql);
+        while ($result = mysqli_fetch_assoc($query)) {
+            echo '<div>';
+                echo '<span>';
+                    echo '<p>'.$result["name"].'</p>';
+                    echo '<p>Klasa - '.$result["class"].'</p>';
+                echo '</span>';
 
-
-            echo '<ul>';
-                echo'<li> Przedmiot: '.$result["subject"].'</li>';
-                echo'<li> Wydawnictwo: '.$result["publisher"].'</li>';
-                echo '<li> Autorzy: ';
-                $authors = explode(",", $result["authors"]);
-                for ($index = 0; $index < count($authors); $index++) {
-                    if ($index != count($authors) - 1) {
-                        echo $authors[$index] . ", ";
-                    } else {
-                        echo $authors[$index];
+                echo '<ul>';
+                    echo'<li> Przedmiot: '.$result["subject"].'</li>';
+                    echo'<li> Wydawnictwo: '.$result["publisher"].'</li>';
+                    echo '<li> Autorzy: ';
+                    $authors = explode(",", $result["authors"]);
+                    for ($index = 0; $index < count($authors); $index++) {
+                        if ($index != count($authors) - 1) {
+                            echo $authors[$index] . ", ";
+                        } else {
+                            echo $authors[$index];
+                        }
                     }
-                }
-            echo '</li></ul>';
-        
-        echo '</div>';
-            // echo '<ul>';
-            //     echo '<li> Przedmiot: '.$result["subject"].'</li>';
-            //     echo '<li> Wydawnictwo: '.$result["publisher"].'</li>';
-            //     echo '<li> Autorzy: ';
-            //    
-            //     for ($index = 0; $index < count($authors); $index++) {
-            //         if ($index != count($authors) - 1) {
-            //             echo $authors[$index] . ", ";
-            //         } else {
-            //             echo $authors[$index];
-            //         }
-            //     }
-            //     echo '</li>';
-            // echo '</ul>';
-        echo '</table>';
-        // echo '<tr>';
-        // echo '<td>' . $result["class"] . '</td>';
-        // echo '<td>' . $result["subject"] . '</td>';
-        // echo '<td>' . $result["name"] . '</td>';
-        // echo '<td>' . $result["publisher"] . '</td>';
-        // echo '<td>';
-        // $authors = explode(",", $result["authors"]);
-        // for ($index = 0; $index < count($authors); $index++) {
-        //     if ($index != count($authors) - 1) {
-        //         echo $authors[$index] . ", <br>";
-        //     } else {
-        //         echo $authors[$index];
-        //     }
-        // }
-        // echo '</td>';
-        // echo '</tr>';
-    }
+                echo '</li></ul>';
+                
+            echo '</div>';
+            echo '</table>';        
+        }
     ?>
 <br>
 <a href="../booklist.json" download>Plik przygotowawczy dla wstawienia książek na stronę</a>
--- src/navigation.php
+++ src/navigation.php
@@ -10,7 +10,6 @@
         <a href="booklist.php">Book List </a>
         <a href="../#przegladaj"> Browse </a>
         <span>
-            <!--//*!if session with "uid" is not set then do shit-->
             <a href="access.php" id="log"><?php  echo !isset($_SESSION["uid"]) ? "Log In" : "Log Out"; ?></a>
             <a href="profile.php" id="prof">Profile</a>
         </span>
