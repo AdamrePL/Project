38f38ec1b79585dd6f1c32492161462c4349ca50
AdamrePL <adam1234federowicz@gmail.com>
2024-03-19 10:18:44 +0100

    added protection for database connection to NOT display sensitive information in case of ANY error (with the connection), additionaly set maximum session time to one hour

--- conf/config.php
+++ conf/config.php
@@ -1,13 +1,36 @@
 <?php
+    #region Database Connection
     define('SERVER', 'localhost');
-    define('USER', 'root');
-    define('PWD', '');
+    define('DB_USER', 'root');
+    define('DB_PWD', '');
     define('DB', 'gielda');
-    
-    $conn = mysqli_connect(SERVER, USER, PWD, DB);
 
-    session_set_cookie_params(0);
+    try {
+        $conn = mysqli_connect(SERVER, DB_USER, DB_PWD, DB);
+    } catch (Exception $err) {
+        echo "Error code: " . mysqli_connect_errno() . " - Failed to connect to MySQL: " . mysqli_connect_error();
+        echo "<font color='crimson'>\nW przypadku wystąpienia tego błedu, proszę powiadomić administratora strony!</font>";
+    }
+
+    #endregion Database Connection
+    
+    #region Session Settings
+
+    /**
+     * Sets session expiration (probably, got to read more about it I guess?) to 1 hour / 5400 is one and a half hour
+     * * If you want to change the time, the param takes time in SECONDS
+     * & In case you don't know how this timeout works, because I also forgot while im typing this, then check out 
+     * @link https://www.php.net/manual/en/ref.session.php
+    */
+    session_set_cookie_params(3600);
     session_start();
     
+    #endregion Session Settings
+    
+    #region Constants
+
     define("SITENAME", "Giełda");
+
+    #endregion Constants
+    
 ?>
