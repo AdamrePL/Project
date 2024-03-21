6826c5565708afa455b5dd7ee66311e0ae0cdb81
PiwkoM <piwkom44@gmail.com>
2024-03-21 12:04:36 +0100

    added admin-tools file allowing for book insertion into `booklist` NOTE: SET RECORD COUNT TO 500 IN PHPMYADMIN!!!!!

---
+++ controllers/admin-tools.php
@@ -0,0 +1,32 @@
+<?php
+if (!isset($_SESSION["isadmin"]) && $_SESSION["admin"]!=true) {
+    header('Location: /');
+    exit(403);
+}
+
+$json_data = file_get_contents("../assets/downloads/booklist.json");
+$json_data = json_decode($json_data);
+foreach ($json_data as $klasa => $value) { // class loop
+    $class = $klasa;
+    echo '<h1>Klasa: ' . $class . '</h1>';
+    foreach ($value as $ksiazka => $dane) { // object loop
+        $dane = get_object_vars($dane); // $dane = json_decode(json_encode($dane), true);
+        echo "<br><br>";
+        $title = $dane["nazwa"];
+        $author = $dane["autorzy"];
+        $publisher = $dane["wydawnictwo"];
+        $subject = $dane["przedmiot"];
+
+        //& note to self: set record count to 500 in phpMyAdmin to avoid 30 minutes of debugging perfectly fine code
+        if (!empty($dane["nazwa"])) {
+            $sql = "INSERT INTO `booklist` VALUES('',?,?,?,?,?)";
+            $stmt = mysqli_stmt_init($conn);
+            mysqli_stmt_prepare($stmt, $sql);
+            mysqli_stmt_bind_param($stmt, 'ssiss', $title, $subject, $class, $author, $publisher);
+            mysqli_stmt_execute($stmt);
+            mysqli_stmt_close($stmt);
+        }
+    }
+}
+
+?>
--- test/test.env.php
+++ test/test.env.php
@@ -1,6 +1,9 @@
 <head><link rel="stylesheet" href="../assets/css/test.env.css"></head>
 
-<?php include_once "../conf/test.php" ?>
+<?php 
+include_once $_SERVER["DOCUMENT_ROOT"]."/conf/test.php"; 
+include_once $_SERVER["DOCUMENT_ROOT"]."/conf/config.php";
+?>
 
 <?php
     echo '<a href="'.ltrim(dirname(str_replace("\\", "/", __DIR__)), $_SERVER["DOCUMENT_ROOT"]) . '">test</a>';
@@ -71,7 +74,3 @@ echo str_replace(" ", "", $_POST["phone"]);
 echo '<br><br><br>';
 print_r($_FILES);
 ?>
-
-<p>
-    Nulla aute excepteur aliquip aliqua do dolore. Magna excepteur aliqua ipsum et amet esse nulla ex ex deserunt esse amet voluptate. Mollit quis occaecat eiusmod esse quis consequat mollit ipsum sunt sunt duis consequat eiusmod. Lorem ipsum consequat exercitation ex qui et anim quis anim in elit sit. Mollit ad elit sunt consequat cupidatat anim excepteur cupidatat eu consectetur officia nisi occaecat velit. Qui elit anim ipsum sint esse in ad mollit quis est labore exercitation ad ea.
-</p>
