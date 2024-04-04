422bd24c5cd80fb425830cadbd635587d8af1189
Nikita Balajew <102317425+chopa113@users.noreply.github.com>
2024-04-04 14:11:30 +0200

    changed to oop

--- class/oferty.php
+++ class/oferty.php
@@ -3,21 +3,58 @@ class Oferty
 {
     private $conn;
     private $table_name_offers = "offers";
+    private $array;
 
 
-    public function __construct($db)
+    public function __construct($conn)
     {
-        $this->conn = $db;
+        $this->conn = $conn;
     }
 
-    public function readOffers(){
-        $queryOffers = "SELECT * FROM" . $this ->table_name_offers . " WHERE `status` = '1' LIMIT 20";
-        $stmtOffers = $this->conn->prepare($queryOffers);
-        $stmtOffers->execute();
+    public function PrintAll(){
+        $sql = "SELECT * FROM `offers` WHERE `status` = '1' LIMIT 20";
+        $query = mysqli_query($this->conn, $sql);
 
-        return $stmtOffers;
+        while ($result = mysqli_fetch_assoc($query)){
+            echo '<div class="offer">';
+                        $sql2 = "SELECT * FROM `products` WHERE `offer-id` =" . $result["id"];
+                        $query2 = mysqli_query($this->conn, $sql2);
+                        $prod = mysqli_num_rows($query2);
+                        
+                        if ($prod > 1) {
+                            echo '<h4 class="offer-title">Pakiet</h4>';
+                            echo '<details>';
+                            echo '<summary>Pakiet zawiera: </summary>';
+                            for ($i = 0; $i < $prod; $i++) {
+                                
+                            }
+                            while ($result2 = mysqli_fetch_assoc($query2)) {
+                                echo $result2["name"] . '<br>';
+                            }
+                            echo '</details>';
+
+                        } else {
+                            $result2 = mysqli_fetch_assoc($query2);
+                            echo '<h4 class="offer-title">'. $result2["name"] .'</h4>';
+                        }
+                        
+                        echo '<details>';
+                        echo '<summary>Dane kontaktowe</summary>';
+                        echo $result["phone"];
+                        echo $result["email"];
+                        echo $result["discord"];
+                        echo '</details>';
+                        
+                        echo '<span class="offer-date">';
+                            echo '<span>oferta utworzona: ' . $result["offer-cdate"] . '</span>';
+                            echo '<span>oferta wygasa: ' . $result["offer-edate"] . '</span>';
+                        echo '</span>';
+                    echo '</div>';
+        }
 
     }
+
+     
 
     
 }
--- index.php
+++ index.php
@@ -109,55 +109,9 @@
         <div class="browse-wrapper">
             <?php
             require_once "class\oferty.php";
-            require_once "class\product.php";
 
             $offers = new Oferty($conn);
-            $offers->readOffers();
-            // $product = new Product();
-            
-
-                // // $search = htmlspecialchars($search, ENT_QUOTES, 'UTF-8');
-                // $sql = "SELECT * FROM `offers` WHERE `status` = '1' LIMIT 20"; //finalne zapytanie
-                // // $sql = "SELECT * FROM `offers` LIMIT 20";
-                // $query = mysqli_query($conn, $sql);
-                // while ($result = mysqli_fetch_assoc($query)) {
-                //     // echo '<a href="?offer='.$result["id"].'">';
-                //     echo '<div class="offer">';
-                //         $sql2 = "SELECT * FROM `products` WHERE `offer-id` =" . $result["id"];
-                //         $query2 = mysqli_query($conn, $sql2);
-                //         $prod = mysqli_num_rows($query2);
-                        
-                //         if ($prod > 1) {
-                //             echo '<h4 class="offer-title">Pakiet</h4>';
-                //             echo '<details>';
-                //             echo '<summary>Pakiet zawiera: </summary>';
-                //             for ($i = 0; $i < $prod; $i++) {
-                                
-                //             }
-                //             while ($result2 = mysqli_fetch_assoc($query2)) {
-                //                 echo $result2["name"] . '<br>';
-                //             }
-                //             echo '</details>';
-
-                //         } else {
-                //             $result2 = mysqli_fetch_assoc($query2);
-                //             echo '<h4 class="offer-title">'. $result2["name"] .'</h4>';
-                //         }
-                        
-                //         echo '<details>';
-                //         echo '<summary>Dane kontaktowe</summary>';
-                //         echo $result["phone"];
-                //         echo $result["email"];
-                //         echo $result["discord"];
-                //         echo '</details>';
-                        
-                //         echo '<span class="offer-date">';
-                //             echo '<span>oferta utworzona: ' . $result["offer-cdate"] . '</span>';
-                //             echo '<span>oferta wygasa: ' . $result["offer-edate"] . '</span>';
-                //         echo '</span>';
-                //     echo '</div>';
-                //     // echo '</a>';
-                // }
+            $offers->PrintAll()
             ?>
         </div>
     </main>
