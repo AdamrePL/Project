bb2d6703b083c4306ef265b9eda168e63467190c
Adamre <116632474+AdamrePL@users.noreply.github.com>
2024-04-15 20:57:18 +0200

    Tabela do weryfikacji email
    przesłałbym cała bazę danych ale 1 z relacji brakuje w niej..
    wiec przesylam wam tylko export tej tabeli
    
    a.. i c**j nie dostaniecie pull requesta :>
    
    "Commit directly to the main branch.
     Some rules will be bypassed by committing directly"

---
+++ db/verify.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 15 Kwi 2024, 20:54
+-- Wersja serwera: 10.4.27-MariaDB
+-- Wersja PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `gielda`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `verify`
+--
+
+CREATE TABLE `verify` (
+  `user-uuid` varchar(34) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `email` text NOT NULL,
+  `token` tinytext NOT NULL,
+  `timeout` datetime NOT NULL,
+  `verified` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `verify`
+--
+ALTER TABLE `verify`
+  ADD UNIQUE KEY `user-uuid` (`user-uuid`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
