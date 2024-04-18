b67af26e16c8be062fa0b767ef6fd9ac41a8f970
derfie <89699885+derfiedev@users.noreply.github.com>
2024-04-18 10:33:50 +0200

    Automatic inactive account deletion

---
+++ controllers/user-expiration.php
@@ -0,0 +1,12 @@
+<?php
+$abspath = $_SERVER["DOCUMENT_ROOT"] . $_SERVER["BASE"];
+
+require_once "$abspath\conf\config.php";
+
+$sql = "DELETE FROM `users` WHERE DATEDIFF(NOW(), `last-login`) >= ? * 30";
+$stmt = mysqli_stmt_init($conn);
+mysqli_stmt_prepare($stmt,$sql);
+mysqli_stmt_bind_param($stmt,"s", USER_EXPIRY_MONTHS);
+mysqli_stmt_execute($stmt);
+mysqli_stmt_close($stmt);
+?>
